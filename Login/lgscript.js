8f22991f894b0607ebc20bdeaa28f47d28605e40
Manoj Kumar <prajapatmanoj609@gmail.com>
2022-02-09 18:39:58 +0530

    add

---
+++ Login/lgscript.js
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+var x = document.getElementById('login');
+var y = document.getElementById('register');
+var z = document.getElementById('btn');
+
+function register() {
+    x.style.left = '-400px';
+    y.style.left = '50px';
+    z.style.left = '110px';
+}
+
+function login() {
+    x.style.left = '50px';
+    y.style.left = '450px';
+    z.style.left = '0px';
+=======
+var x = document.getElementById('login');
+var y = document.getElementById('register');
+var z = document.getElementById('btn');
+
+function register() {
+    x.style.left = '-400px';
+    y.style.left = '50px';
+    z.style.left = '110px';
+}
+
+function login() {
+    x.style.left = '50px';
+    y.style.left = '450px';
+    z.style.left = '0px';
+>>>>>>> 798011ee03272b4853619e44e2e210f8c6173a22
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# SellersBay
+=======
+# sellersbay
+>>>>>>> 798011ee03272b4853619e44e2e210f8c6173a22
